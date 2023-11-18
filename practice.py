13133f2376c3c8e65c0142a9df54f3f44cb96e1a
minsunn94 <minsunn94@naver.com>
2023-11-18 19:27:22 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,17 @@
+#패키지 = 모듈의 집합
+
+# import travel.thailand.
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel. thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import vetnam
+# trip_to = vetnam.VetnamPackage()
+# trip_to.detail()
+
+from travel import*
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
